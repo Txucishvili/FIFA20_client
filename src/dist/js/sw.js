e528408a428e5923c49b34cf99e625c11db5a8c2
txucishvili <toc.xucishvili@gmail.com>
2019-10-25 23:02:21 +0400

    - add: manifest.webmanifest

--- src/dist/js/sw.js
+++ src/dist/js/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('video-store').then(function(cache) {
       return cache.addAll([
-        '/',
+        '/FIFA20_client/src/index.html',
         './app.bundle.js',
         './a2hs.bundle.js',
       ]);
